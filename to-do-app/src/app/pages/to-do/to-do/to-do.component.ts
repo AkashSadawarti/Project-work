f2a3d3c8ad3b8db66ac64b8f3e4d0b0746a4cded
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-06-06 10:47:54 +0530

    to do app update

--- to-do-app/src/app/pages/to-do/to-do/to-do.component.ts
+++ to-do-app/src/app/pages/to-do/to-do/to-do.component.ts
@@ -1,91 +1,135 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ToDo } from '../../model/to-do.model';
-import { NgForm } from '@angular/forms'
 import { ToDoService } from '../../service/to-do.service';
-import { finalize } from 'rxjs';
-import { materialize } from 'rxjs';
-// import {MatTableModule} from '@angular/material/table';
+import { finalize} from 'rxjs';
+
+
+
 
 @Component({
   selector: 'app-to-do',
   templateUrl: './to-do.component.html',
   styleUrls: ['./to-do.component.css']
 })
-export class ToDoComponent implements OnInit{
+export class ToDoComponent implements OnInit {
 
-  allTasks : ToDo[] = []
-  _todo : ToDo = {};
+  allTasks: ToDo[] = []
+  _todo: ToDo = {};
 
-  requestPayload : ToDo = {};
-
-  loginBtnSpinner: boolean =false;
+  requestPayload: ToDo = {};
+  status = ["Todo", "Pending", "In Progress", "Done"]
+  loginBtnSpinner: boolean = false;
+  router: any;
+  registerSucess:boolean = false;
 
   ngOnInit(): void {
-    this.getList(); 
+    this.getList();
   }
 
   constructor(
-    private todoService : ToDoService ){
+    private todoService: ToDoService) {
     console.log("todo service Invoked")
   }
 
-  
-  saveChanges(){
+  openModal() {
+    const modalDiv = document.getElementById('myModal');
+    if (modalDiv != null) {
+      modalDiv.style.display = 'block';
+    }
+    this._todo.title = ""
+    this._todo.date = ""
+    this._todo.day = ""
+    this._todo.status = ""
+  }
+
+  closeModal() {
+    const modalDiv = document.getElementById('myModal');
+    if (modalDiv != null) {
+      modalDiv.style.display = 'none';
+    }
+  }
+
+  saveChanges() {
     this.requestPayload = this.todoModel;
     console.log(JSON.stringify(this.todoModel));
-    if(this._todo.id == 0){
-      this.createTask(this.requestPayload)
-      console.log("task Created succesfully.");
-    }else{
-      this.editTask(this.todoModel)
-      console.log("task Updated succesfully.");
-    }
-    
+    this.createTask(this.requestPayload)
+    console.log("task Created succesfully.");
+    this.closeModal();
   }
-  
-  createTask( todos : ToDo){
+
+  createTask(todos: ToDo) {
     console.log(JSON.stringify(todos));
     this.todoService
-    .createTask(todos).pipe(finalize(()=>(this.loginBtnSpinner = false))).subscribe((data : any) =>{
-      this.ngOnInit()   
-    });
+      .createTask(todos).pipe(finalize(() => (this.loginBtnSpinner = false))).subscribe((data: any) => {
+        this.ngOnInit();
+        this.registerSucess=true;
+      });
+  }
+
+  openModal2() {
+    const modalDiv = document.getElementById('myModal2');
+    if (modalDiv != null) {
+      modalDiv.style.display = 'block';
+    }
+  }
+
+  closeModal2() {
+    const modalDiv = document.getElementById('myModal2');
+    if (modalDiv != null) {
+      modalDiv.style.display = 'none';
+    }
   }
  
+
+  // getTaskById(taskId : ToDo){
+  //   this.todoService.getTaskById(taskId).subscribe((res: any) => {
+  //     console.log("Task Id processed succesfully.");
+  //   });
+  // }
+
+  updateChanges() {
+    this.todoModel = this.todoModel;
+    console.log(JSON.stringify(this.todoModel));
+    this.editTask(this.todoModel)
+    console.log("task updated succesfully.");
+    this.closeModal();
+  }
+
   // Edit Task
-  editTask(id : ToDo){
-    console.log(JSON.stringify(id));
+  editTask(task : ToDo) {
     this.todoService
-    .updateTask(id).subscribe((res : any) => {
-      this.todoModel = res;
-    })
+      .updateTask(task).subscribe((res: any) => {
+        this.getList();
+      })
   }
 
   // Delete Task
-delete(task : ToDo){
-  console.log(this._todo.id);
-  console.log(JSON.stringify(task));
+  delete(task: ToDo) {
+    console.log(this._todo.id);
+    console.log(JSON.stringify(task));
     this.todoService
-    .deleteTask(task).subscribe((res : any) => {
-      alert("Task deleted succesfully.")
-    })
-}
+      .deleteTask(task).subscribe((res: any) => {
+        console.log("Task deleted succesfully.");
+        this.getList();
+      })
+  }
 
   //getter
-public get todoModel() : ToDo {
-   return this._todo;
-}
- // setter
- public set todoModel(value :  ToDo){
-  this._todo = value;
- } 
+  public get todoModel(): ToDo {
+    return this._todo;
+  }
+  // setter
+  public set todoModel(value: ToDo) {
+    this._todo = value;
+  }
 
-  getList(){
-     this.todoService.getList().subscribe((response :  any) => {
+  getList() {
+    this.todoService.getList().subscribe((response: any) => {
       console.log(response);
       this.allTasks = response
-     },err => {
+    }, err => {
       alert('api not working')
-     });
+    });
   }
 }
 
